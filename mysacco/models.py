c5084975c86ff20b016821796ef855c2d14aeefb
Samuel Maiko <sammymaiko721@gmail.com>
2023-09-04 17:07:01 +0300

    Added the models to the db

--- mysacco/models.py
+++ mysacco/models.py
@@ -4,3 +4,33 @@ from sqlalchemy.ext.declarative import declarative_base
 engine=create_engine('sqlite:///thesacco.db')
 Base=declarative_base()
 
+
+class Sacco(Base):
+    
+    __tablename__='saccos'
+    
+    id= Column(Integer(), primary_key=True)
+    name=Column(String())
+    manager=Column(String())
+    created_at=Column(DateTime(), server_default=func.now())
+    updated_at=Column(DateTime(), onupdate=func.now())
+
+class Shuttle(Base):
+    
+    __tablename__='shuttles'
+    
+    id= Column(Integer(), primary_key=True)
+    type=Column(String())
+    number_plate=Column(String())
+    created_at=Column(DateTime(), server_default=func.now())
+    updated_at=Column(DateTime(), onupdate=func.now())
+    
+class Member(Base):
+    
+    __tablename__='members'
+    
+    id= Column(Integer(), primary_key=True)
+    first_name=Column(String())
+    last_name=Column(String())
+    created_at=Column(DateTime(), server_default=func.now())
+    updated_at=Column(DateTime(), onupdate=func.now())
